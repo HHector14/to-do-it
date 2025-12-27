10b32291462904b1f703aee5b31e377a2b74bd56
HHector14 <hernandezgarcia.hectorf@gmail.com>
2025-12-26 20:35:52 -0600

    LocalStorageTask type added in hook useLS

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import type { LocalStorageTask } from "../types/hooks";
 
-export function useLocalStorage(key, initialValue) {
-  const [value, setValue] = useState(
-    () => {
+
+export function useLocalStorage<T>({
+  key,
+  initialValue
+}: LocalStorageTask<T>): [T, React.Dispatch<React.SetStateAction<T>>] {
+  
+  const [value, setValue] = useState(() => {
       const data = localStorage.getItem(key)
-      return data ? JSON.parse(data) : initialValue
+      return data ? JSON.parse(data)  : initialValue
     }
   )
   useEffect(() => {
