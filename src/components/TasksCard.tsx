c3222c648df608e04c3f6f384009384f1e8576ec
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-05 12:20:26 -0600

    new lineloader in task card

--- src/components/TasksCard.tsx
+++ src/components/TasksCard.tsx
@@ -2,16 +2,22 @@ import Task from "./Task";
 import '../App.css'
 import useHandleTask from "../hooks/useHandleTask";
 import type { TaskCard } from "../types/components";
-
+import { LinearProgress } from "@mui/material";
 export default function TasksCard({ tasks, setTasks }: TaskCard) {
   const { getTotalTasksFinished } = useHandleTask({tasks, setTasks});
+   const progress = getTotalTasksFinished() * 100 / tasks.length || 0;
   return (
     <div className='section'>
       <h1>TO DO IT</h1>
       {tasks.length == 0 ?
           <p style={{ fontSize: '20px', fontWeight: "bold" }}>NO TASKS YET</p>
           :
-          <p style={{textAlign:"right", marginRight: '20px', fontWeight: '700', color: '#bebabaff'}}>FINISHED {getTotalTasksFinished()}/{tasks.length}</p>
+ <>
+          <LinearProgress variant="determinate" value={progress} />
+          <p className="finished-count">FINISHED {getTotalTasksFinished()}/{tasks.length}</p>
+
+          </>
+
 
       }
       {tasks.map((item, index) => (
